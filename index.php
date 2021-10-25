238e1fd229ac8dff6213b5470cad1c918e821491
Michele Greco <mitchgrek@gmail.com>
2021-10-25 19:24:23 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!-- <!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Snake Game</title>
+  <script src="p5/p5.js" type="text/javascript"></script>
+  <script src="p5/p5.dom.js" type="text/javascript"></script>
+  <script src="scripts/sketch.js" type="text/javascript"></script>
+  <script src="scripts/field.js" type="text/javascript"></script>
+  <script src="scripts/snake.js" type="text/javascript"></script>
+  <script src="scripts/food.js" type="text/javascript"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <div id="snake-field" class="snakeField">
+    <div class="score" id="score">0</div>
+  </div>
+
+</body>
+
+</html> -->
+
+
+<!DOCTYPE html>
+<?php
+include_once("../shared/navbar.php");
+include_once("php/form.php");
+include_once("php/footerTop.php");
+include_once("php/footerBottom.php");
+?>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<link rel="icon" href="../img/favicon.ico">
+	<title>miitchPls</title>
+	
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/style.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,900" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	
+	
+</head>
+<body class="bg-dark">
+  <?php navbar() ?>
+</body>
+</html>
